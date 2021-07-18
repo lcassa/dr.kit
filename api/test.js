e8bc7e7066188262a0be1843c9d0738df20de383
Leandro Cassa <leandro.cassa@digibee.com.br>
2021-07-18 07:19:52 -0300

    file support experiment

--- api/test.js
+++ api/test.js
@@ -1,5 +1,32 @@
 export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ name: 'John Doe' });
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'application/json');
+  // res.json({ name: 'John Doe' });
+  try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            //avatar.mv('./uploads/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
